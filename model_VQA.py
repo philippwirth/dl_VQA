1257413b2553923a40008b68c792a4216231e5df
pwirth <pwirth@ethz.ch>
2017-12-08 15:15:11 +0100

    add a few functions

--- model_VQA.py
+++ model_VQA.py
@@ -1,4 +1,24 @@
-class Model:
+class VQAModel:
 
 	def __init__(self):
+		pass
+
+	def build_model(self):
+		# build a model to be trained to answer questions (use for training)
+		pass
+
+	def build_generator(self):
+		# build a model to answer questions to images (use for testing)
+		pass
+
+	def _embed_question(self):
+		# embed question somehow
+		pass
+
+	def _embed_image(self):
+		# embed image using bidirectional lstm
+		pass
+
+	def _fuse(self):
+		# fuse embedded question and image to 1 vector
 		pass
